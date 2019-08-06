47a44d842cec4cfd7c46feb464e59de4ffa69026
jgodet <jgodet@unistra.fr>
2019-08-06 14:49:38 +0200

    package for GMRC shiny stats

---
+++ R/loadPakage.r
@@ -0,0 +1,34 @@
+# loadPakage.r
+# written by JuG
+# August 06 2019
+
+
+#' Do something
+#' @author JuG
+#' @description 
+#' @param 
+#' @details 
+#' @examples 
+#'
+#'
+#' @return 
+#' @export
+
+
+if(!require(ggplot2)){install.packages('ggplot2')}; library(ggplot2)
+if(!require(shiny)){install.packages('shiny')}; library(shiny)
+if(!require(dplyr)){install.packages('dplyr')}; library(dplyr)
+#library(knitr)
+#library(FactoMineR)
+if(!require(pROC)){install.packages('pROC')}; library(pROC)
+if(!require(irr)){install.packages('irr')}; library(irr)
+if(!require(moments)){install.packages('moments')}; library(moments)
+if(!require(DT)){install.packages('DT')}; library(DT)
+if(!require(gdata)){install.packages('gdata')}; library(gdata )
+if(!require(stringr)){install.packages('stringr')}; library(stringr )
+if(!require(boot)){install.packages('boot')}; library(boot )
+# library(readr)
+if(!require(xtable)){install.packages('xtable')}; library(xtable )
+if(!require(shinyFiles)){install.packages('shinyFiles')}; library(shinyFiles)
+
+if(!require(devtools)){install.packages('devtools')}; library(devtools)
