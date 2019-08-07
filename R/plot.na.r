2354c3900d85e30ab0d858ecd692d6ecd34f1233
jgodet <jgodet@unistra.fr>
2019-08-07 15:51:24 +0200

    update plot.na

--- R/plot.na.r
+++ R/plot.na.r
@@ -3,19 +3,22 @@
 # August 06 2019
 
 
-#' Do something
+#' Graphical representation of  missing values present in a data.frame 
 #' @author JuG
-#' @description 
-#' @param 
+#' @description  Graphical representation of  missing values present in a data.frame 
+#' @param data_in data frame
 #' @details 
 #' @examples 
-#'
-#'
+#' dtf <- data.frame(varA = rnorm(10), varB = sample(x = LETTERS[1:2], size = 10, replace=T), varC = sample(c(1:9,NA), size=10) )
+#' plot.na(dtf)
+#' plot.na(dtf, title = "Titre")
 #' @return 
 #' @export
 
 
 plot.na <- function(data_in, title = NULL){
+  if(!require(ggplot2)){install.packages('ggplot2')}
+  require(ggplot2)
   temp_df <- as.data.frame(ifelse(is.na(data_in), 1, 0))
   temp_df <- temp_df[,order(colSums(temp_df))]
   data_temp <- expand.grid(list(x = 1:nrow(temp_df), y = colnames(temp_df)))
